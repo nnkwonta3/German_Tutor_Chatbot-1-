7562198e4d898a79ff7fe22bffd581de45393878
frances_nkwonta <nnkwonta3@gatech.edu>
2024-10-24 14:19:42 -0400

    Update App.tsx

--- ChatBot/frontend/src/App.tsx
+++ ChatBot/frontend/src/App.tsx
@@ -1,24 +1,29 @@
-import Header from "./components/Header.tsx";
-import {Route, Routes} from "react-router-dom";
-import Home from "./pages/Home.tsx";
-import Login from "./pages/Login.tsx";
-import Signup from "./pages/Signup.tsx";
-import Chat from "./pages/Chat.tsx";
-import NotFound from "./pages/NotFound.tsx";
-import {useAuth} from "./context/AuthContext.tsx";
-
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Chat from "./pages/Chat";
+import NotFound from "./pages/NotFound";
+import { useAuth } from "./context/AuthContext";
+import Footer from "./components/footer/Footer";
 function App() {
-  return <main>
-            <Header/>
-              <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/signup" element={<Signup />} />
-                  <Route path="/chat" element={<Chat />} />
-                  <Route path="*" element={<NotFound />} />
-              </Routes>
-          </main>;
+    const auth = useAuth();
 
+    return (
+        <main>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                {auth?.isLoggedIn && auth.user && (
+                    <Route path="/chat" element={<Chat />} />
+                )}
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </main>
+    );
 }
 
-export default App
+export default App;
